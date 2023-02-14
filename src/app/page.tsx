1694e0430adcad97f5de5b074b8ab73f6921dcf6
Neander de Souza <neander.faria@hotmail.com>
2023-02-14 16:32:59 -0300

    Fetch de dados nas páginas

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,14 @@
 import Link from 'next/link'
 
-export default function Home() {
+export default async function Home() {
+  const response = await fetch('https://api.github.com/users/neanderdev')
+  const user = await response.json()
+
   return (
     <div>
-      <h1>Hello World</h1>
+      <h1>Home</h1>
+
+      <pre>{JSON.stringify(user, null, 2)}</pre>
 
       <Link href="/app">Dashboard</Link>
     </div>
