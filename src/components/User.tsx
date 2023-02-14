1b98a563df8914ec55454aca3d7f86a4f9214084
Neander de Souza <neander.faria@hotmail.com>
2023-02-14 16:53:31 -0300

    Acessando cookies e headers

--- src/components/User.tsx
+++ src/components/User.tsx
@@ -1,3 +1,5 @@
+import { cookies, headers } from 'next/headers'
+
 import { Repo } from './Repo'
 
 // export const revalidate = 30
@@ -11,9 +13,17 @@ export async function User() {
   })
   const user = await response.json()
 
+  const userCookies = cookies()
+  const userHeaders = headers()
+
   return (
     <div>
       <h1>User</h1>
+
+      <pre>{JSON.stringify(userCookies.getAll(), null, 2)}</pre>
+      <pre>{JSON.stringify(userCookies.get('pegar_cookie'), null, 2)}</pre>
+
+      <pre>{JSON.stringify(userHeaders.entries(), null, 2)}</pre>
 
       <pre>{JSON.stringify(user, null, 2)}</pre>
 
