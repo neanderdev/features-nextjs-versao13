8d3d9fa74f40bca91ace043b0b59f9cfea7fa52b
Neander de Souza <neander.faria@hotmail.com>
2023-02-14 15:40:54 -0300

    Novo diretório app

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,11 @@
+import Link from 'next/link'
+
 export default function Home() {
-  return <h1>Hello World</h1>
+  return (
+    <div>
+      <h1>Hello World</h1>
+
+      <Link href="/dashboard">Dashboard</Link>
+    </div>
+  )
 }
